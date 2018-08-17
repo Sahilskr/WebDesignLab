e6826f972767506804550e840c2b037fc70abea2
Your Name <tahapipe90@gmail.com>
2018-08-17 11:16:38 +0530

    d

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(function(){
+                                      $(function(){
 	$('#rollno').on('blur',function(){
 		if(!/(\d{2}((CO)|(DCO)|(EE)|(DEE)|(EX)|(DEX)|(ME)|(DME)|(CE)|(DCE)|(CES)|(DCES))\d{2,3})/i.test(this.value)){
 			alert("Invalid Roll no");
